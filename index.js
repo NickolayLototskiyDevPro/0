62aac82999254f45b9cf056d2d37bc4b87c5baa4
Irina Korotkova <irina-kim@ukr.net>
2017-11-12 10:20:34 +0200

    exam2 final version

--- index.js
+++ index.js
@@ -12,17 +12,27 @@
 //     roleName: number
 // }
 
-const project = {
-    participants: [],
-    pricing: { },
-    isBusy: boolean,
+let ProjectModule = (function () {
+    
+    const project = {
+        participants: [],
+        pricing: {},
+        isBusy: false,
 
     /* implement initialization of the object */
     /* participants - predefined array of participants */
     /* pricing - predefined object (keyvalue collection) of pricing */
     init(participants, pricing) {
-        this.participants = participants;
-        this.pricing = pricing;
+        this.isBusy = true;
+        {
+        if(Array.isArray(participants)){
+            if(participants.length == 0 || participants.every((item) => 'seniorityLevel' in item)) {
+                this.participants = participants;
+            }
+        }
+        if(typeof pricing == 'object' && pricing != null) this.pricing = pricing;
+        this.isBusy = false;
+        }
      },
 
  
@@ -34,55 +44,104 @@ const project = {
     findParticipant(functor, callbackFunction) { 
         let me = this;
         me.isBusy = true;
-        setTimeout(function(){
-             let result = this.participants.filter(functor);
-             me.isBusy = false;
-             if(callbackFunction){
-                 callbackFunction(result);
-             }
-        }, 200);
+        setTimeout(() => {
+            let result = this.participants.find(functor);
+            (result === undefined) ? callbackFunction(null) : callbackFunction(result);
+            me.isBusy = false;
+        }, 100);
     },
+
 
     /* pass array of found participants into callback */
     /* functor - function that will be executed for elements of participants array */
     /* callbackFunction - function that will be executed with array of found participants as argument or empty array if not */
     /* callbackFunction (participantsArray) => {} */
-    findParticipants(functor, callbackFunction) { },
+    findParticipants(functor, callbackFunction) { 
+        let me = this;
+        me.isBusy = true;
+        setTimeout(() => {
+            let participantsArray = this.participants.filter(functor);
+            callbackFunction (participantsArray); 
+            me.isBusy = false;
+        }, 100);
+    },
 
     /* push new participant into this.participants array */
     /* callbackFunction - function that will be executed when job will be done */
     /* (err) => {} */
-    addParticipant(participantObject, callbackFunction) { },
+    addParticipant(participantObject, callbackFunction) {
+        let me = this;
+        me.isBusy = true;
+        setTimeout(() => {
+            try{
+                if(typeof participantObject === 'object' && 'seniorityLevel' in participantObject){
+                    this.participants.push(participantObject);
+                    callbackFunction();     
+                }else{
+                   throw new Error('Wrong Data!');
+                }
+            }catch(err){
+                callbackFunction(err);   
+                }
+            me.isBusy = false;
+        }, 100);
+     },
 
     /* push new participant into this.participants array */
     /* callback should receive removed participant */
     /* callbackFunction - function that will be executed with object of removed participant or null if participant wasn't found when job will be done */
-    removeParticipant(participantObject, callbackFunction) { },
+    removeParticipant(participantObject, callbackFunction) {
+        let me = this;
+        me.isBusy = true;
+        setTimeout(() => {
+            let removedParticipant = null;
+            for (var i = 0; i < this.participants.length; i++){
+                if(this.participants[i] == participantObject){
+                    removedParticipant = this.participants.splice(i, 1)[0];
+                    break;
+                    }                        
+                }
+        callbackFunction(removedParticipant); 
+            me.isBusy = false;
+        }, 100);
+     },
 
     /* Extends this.pricing with new field or change existing */
     /* callbackFunction - function that will be executed when job will be done, doesn't take any arguments */
-    setPricing(participantPriceObject, callbackFunction) { },
+    setPricing(participantPriceObject, callbackFunction) { 
+        let me = this;
+        me.isBusy = true;
+        setTimeout(() => {
+            Object.assign(this.pricing, participantPriceObject);
+            callbackFunction();
+            me.isBusy = false;
+        }, 100);
+    },
 
     /* calculates salary of all participants in the given period */
     /* periodInDays, has type number, one day is equal 8 working hours */
-    calculateSalary(periodInDays) { }
-}
+    calculateSalary(periodInDays) { 
+        const workDay = 8;
+        let salary = this.participants.reduce((sum, i) => {
+            return sum + this.pricing[i.seniorityLevel] * periodInDays * workDay;
+        }, 0);
+
+        if (!isNaN(salary)) {
+            return salary;
+        } else {
+            throw new Error('Error')
+        }
+      }
+    }
 
 
+    let instance,
+    createInstance = () => project,
+    getInstance = () => instance || (instance = createInstance());
 
+    return getInstance();
+})();
 
-
-
-// Все методы которые содержат callbackFunction в списке аргументов должны выполнять свою работу в setTimeout и возвращать значение в данный коллбек.
-
-// Все методы которые содержат callbackFunction в списке аргументов должны устанавливать isBusy в значение true когда начинают работу
-// и в значение false когда заканчивают работу.
-
-// Если isBusy установлено в true метод не должен выполнять какую либо работу и сразу же вернуть false.
-
-// Объект project должен быть реализован как синглтон.
-
-// !!! Я ожидаю что Вы запушите файл index.js в корень предоставленного репозитория в следующем виде
 
 /* реализация */
 module.exports = {
